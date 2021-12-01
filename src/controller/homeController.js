1875bc0d3a0896ee0239be503d37aa8b833e468f
Hashirama <88428250+LBTZ7@users.noreply.github.com>
2021-12-01 22:43:48 +0700

    Delete and edit user information

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -22,12 +22,37 @@ let createNewUser = async (req, res) => {
   let {firstName, lastName, email, address} = req.body
   await pool.execute('insert into users(firstName, lastName, email, address) values (?, ?, ?, ?)', 
   [firstName, lastName, email, address])
-  return res.redirect('/')
+  return res.redirect('/') // reload
 }
 
+let deleteUser = async (req, res) => {
+    // delete existent user and update the SQL
+  let userId = req.body.userId
+  await pool.execute('delete from users where id = ?', [userId])
+  return res.redirect('/') // reload
+}
+
+let getEditPage = async (req, res) => {
+  let UserId = req.params.id // get user id
+  let [user] = await pool.execute('select * from users where id = ?', [UserId]) // get the user from SQL
+  return res.render('./update.ejs',  {dataUser: user[0]})
+}
+
+let postUpdateUser = async (req,res) => {
+  // Edit user informatin and update in SQL
+  let {firstName, lastName, email, address, id} = req.body
+
+  await pool.execute('update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?', 
+  [firstName, lastName, email, address, id])
+
+  return res.redirect('/') // reload
+}
 
 module.exports ={
   getHomePage,
   getDetailPage,
-  createNewUser
+  createNewUser,
+  deleteUser,
+  getEditPage,
+  postUpdateUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -10,8 +10,14 @@ let router = express.Router()
 
 const initWebRoute = (app) =>{
     router.get('/', homeController.getHomePage)
+
     router.get('/detail/user/:id', homeController.getDetailPage)
     router.post('/create-new-user', homeController.createNewUser)
+    router.get('/edit-user/:id', homeController.getEditPage)
+
+    router.post('/delete-user', homeController.deleteUser)
+    router.post('/update-user', homeController.postUpdateUser)
+
     router.get('/about', (req, res) => {
         res.send(`I'm Thomas!`)
     })
