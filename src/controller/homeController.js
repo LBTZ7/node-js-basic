ab5975f33b48747c8d6aa76fd6647d4374ac94ae
Hashirama <88428250+LBTZ7@users.noreply.github.com>
2021-12-04 21:58:23 +0700

    Handling Upload File with Express.js and Multer

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,6 @@
 import pool from '../configs/connectDB'
+import multer from 'multer'
+
 
 let getHomePage = async (req, res) => {
       const [rows, fields] = await pool.execute('SELECT * FROM users');
@@ -48,11 +50,57 @@ let postUpdateUser = async (req,res) => {
   return res.redirect('/') // reload
 }
 
+let getUploadFilePage = async (req,res) => {
+  // redirect to UPload File window
+    res.render('uploadFile.ejs')
+}
+
+let handleUploadFile = async (req,res) => {
+    // req.file contains information of uploaded file
+    // req.body contains information of text fields, if there were any
+
+    if (req.fileValidationError) {
+        return res.send(req.fileValidationError);
+    }
+    else if (!req.file) {
+        return res.send('Please select an image to upload');
+    }
+
+    // Display uploaded image for user validation
+    res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+
+}
+
+let handleUploadMultipleFiles = async (req,res) => {
+
+        if (req.fileValidationError) {
+              return res.send(req.fileValidationError);
+          }
+          else if (!req.files) {
+              return res.send('Please select an image to upload');
+          }
+
+        let result = "You have uploaded these images: <hr />";
+        const files = req.files;
+        let index, len;
+
+        // Loop through all the uploaded images and display them on frontend
+        for (index = 0, len = files.length; index < len; ++index) {
+            result += `<img src="/image/${files[index].filename}" width="300" style="margin-right: 20px;">`;
+        }
+        result += '<hr/><a href="/upload">Upload more images</a>';
+        res.send(result);
+
+}
+
 module.exports ={
   getHomePage,
   getDetailPage,
   createNewUser,
   deleteUser,
   getEditPage,
-  postUpdateUser
+  postUpdateUser,
+  getUploadFilePage ,
+  handleUploadFile,
+  handleUploadMultipleFiles
 }
