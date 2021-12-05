09556028b9658be44881c4692f878ad1ee0227c1
Hashirama <88428250+LBTZ7@users.noreply.github.com>
2021-12-05 21:25:11 +0700

    complete User and File management

--- src/server.js
+++ src/server.js
@@ -2,14 +2,16 @@ import express from 'express'
 import configViewEngine from './configs/viewEngine'
 import initWebRoute from './route/web';
 import initAPIRoute from './route/api';
-// import connection from './configs/connectDB'
+
 
 require('dotenv').config()
+var morgan = require('morgan')
+
 
 const app = express()
 const port = process.env.PORT
 
-// call this to use method 'POST' and 'GET'
+app.use(morgan('combined'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -21,6 +23,11 @@ initWebRoute(app)
 
 // init API route
 initAPIRoute(app)
+
+//handle 404 not found
+app.use((req,res) => {
+    return res.render('404.ejs')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
